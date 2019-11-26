1068b4aeb190d3d339e436bd952954f1625d7133
Nelson <nelsonsnake_@hotmail.com>
2019-11-26 15:47:06 -0600

    commit inicial

---
+++ src/main/java/com/usertest/UsertestApplication.java
@@ -0,0 +1,13 @@
+package com.usertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usertest/UsertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
